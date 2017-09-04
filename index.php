2df890f3f606f8a406d8356dd060fb14534c7b0f
akseli <akuistio@gmail.com>
2017-09-04 15:02:22 +0300

    test/time routes, composer file, .gitignore

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
 
-echo "Hello World!";
+$app = new Silex\Application();
+date_default_timezone_set('Europe/Helsinki');
+
+$app->get('/', function () {
+   return "StormChat API";
+});
+
+$app->post('/', function () {
+    return "StormChat API";
+});
+
+$app->get('/get_time', function () use ($app) {
+    return date('Y-m-d H:i:s');
+});
+
+$app->post('/get_time', function () use ($app) {
+    return json_encode(['time' => date('Y-m-d H:i:s')]);
+});
+
+$app->run();
