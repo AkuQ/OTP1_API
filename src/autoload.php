a9cd972da411dd85a299710de30fb45b10b7fa52
akseli <akuistio@gmail.com>
2017-09-11 09:17:55 +0300

    fixed autoloader path

--- src/autoload.php
+++ src/autoload.php
@@ -5,6 +5,7 @@ date_default_timezone_set('Europe/Helsinki');
 
 spl_autoload_register(function($name) {
     if(substr($name,0,9) == 'StormChat') {
+        $name = substr($name, 10);
         $file = str_replace('\\', '/', __DIR__."/$name.php" );
         if (file_exists($file)) /** @noinspection PhpIncludeInspection */
             include $file;
