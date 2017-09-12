55d8cfe26f70e271254800e9782253d4d3233eec
Arto Jussilainen <artojussilainen@gmail.com>
2017-09-12 14:12:51 +0300

    DB_Handler and reordering

--- src/DB_Handler.php
+++ src/DB_Handler.php
@@ -1,38 +1,56 @@
 <?php
+
 namespace StormChat;
 
-class Controller {
+
+class DB_Handler {
+
+
+    private $passwd = "";
+    private $user = "";
+
+    function connect() {
+        $file = fopen("pass.txt", "r");
+        $this->user = fgets($file);
+        $this->passwd = fgets($file);
+
+        $connection = mysqli_connect("localhost", "TEST_OTP_API", $this->passwd, $this->user);
+        return $connection;
+    }
 
     function get_groups() {
         $date = getdate();
         $year = $date['year'];
         $month = $date['mon'];
         $day = $date['mday'];
-        $connection = DB_Handler.get_connection();
-       // handler->selec(table, [col1, col2], "col1 > 0 AND col2 = 0")
+        $connection = $this->connect();
+        // handler->selec(table, [col1, col2], "col1 > 0 AND col2 = 0")
 
-        $sql = "SELECT chat_id 
+        $sql = "SELECT name 
         FROM chat WHERE created BETWEEN '$year-$month-$day 23:59:59' AND '$year-".($month - 1)."-$day 00:00:00'";
 
-        //$result = $connection->query($sql);
-        $result = DB_Handler.select($sql);
-        $chat_ids = [];
+        $groups = [];
+        $result = $connection->query($sql);
         while ($row = $result->fetch_assoc()) {
-            array_push($chat_ids, $row["chat_id"]);
+            array_push($groups, $row["name"]);
         }
-        $connection = close();
-        return $chat_ids;
+
+        return $groups;
+
     }
 
     function create_user($name, $password) {
         //palauta id ja token (tehty)
-
+        $token = sha1($password);
         $connection = DB_Handler.get_connection();
         $created = date("Y-m-d h:i:s");
-        $sql = $connection->prepare("INSERT INTO user (name, token, created) VALUES (?, ?, ?)");
-        $sql->bind_param("ssis", $name, $token, $created);
+        $sql = $connection->prepare("INSERT INTO user (user_id, name, token, created) VALUES (?, ?, ?, ?)");
+        $sql->bind_param("ssis", NULL, $name, $token, $created);
         $sql->execute();
+        $id= $connection->insert_id;
         $connection->close();
+
+        return [$token, $id];
     }
 
     function get_messages($user_id, $chat_id) {
@@ -60,4 +78,6 @@ class Controller {
         $connection->close();
 
     }
+
+
 }
