8d0ab7681d0d537c006dc7050735e07c9161a658
akseli <akuistio@gmail.com>
2017-09-12 12:57:20 +0300

    api request-response handler (untested) and placeholder routes

--- index.php
+++ index.php
@@ -1,9 +1,41 @@
 <?php
+use StormChat\API_Handler;
+use StormChat\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/src/autoload.php';
 
+
 $app = new Silex\Application();
 
+# SERVICES:
+
+$app['api_handler'] = function () {
+    return new API_Handler();
+};
+
+$app['controller'] = function () {
+    return new Controller();
+};
+
+
+
+# MIDDLEWARE:
+
+$app->before(function (Request $request) use ($app){
+    /** @var API_Handler $api_handler */
+    $api_handler = $app['api_handler'];
+    $api_handler->parse_request($request);
+});
+
+$app->after(function (Request $request, Response $response) use ($app){
+    $response->setCharset('UTF-8');
+});
+
+
+# ROUTES:
 
 $app->get('/', function () {
    return "StormChat API";
@@ -13,15 +45,37 @@ $app->post('/', function () {
     return "StormChat API";
 });
 
-$app->get('/get_time', function () use ($app) {
+$app->get('/get_time', function (Request $request) use ($app) {
     return date('Y-m-d H:i:s');
 });
 
-$app->post('/get_time', function () use ($app) {
-    return json_encode(['time' => date('Y-m-d H:i:s')]);
+$app->post('/get_time', function (Request $request) use ($app) {
+    return $app['api_handler']->respond(
+        $request,
+        [$app['controller'], 'get_time']
+    );
 });
 
+$app->post('/users/create', function (Request $request) use ($app) {
+    return $app['api_handler']->respond(
+        $request,
+        [$app['controller'], 'create_user']
+    );
+});
 
+$app->post('/rooms/list', function (Request $request) use ($app) {
+    return $app['api_handler']->respond(
+        $request,
+        [$app['controller'], 'list_rooms']
+    );
+});
+
+$app->post('/rooms/create', function (Request $request) use ($app) {
+    return $app['api_handler']->respond(
+        $request,
+        [$app['controller'], 'create_room']
+    );
+});
 
 
 $app->run();
---
+++ src/API_Handler.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akseli
+ * Date: 9.9.2017
+ * Time: 14:00
+ */
+
+namespace StormChat;
+
+
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
+
+class API_Handler
+{
+    public function __construct() {
+    }
+
+    public function parse_request(Request $request) {
+        $data = json_decode($request->getContent(), true);;
+        if ($data === null) {
+            throw new Exception("Bad JSON");
+        }
+        else {
+            $request->request->replace($data);
+        }
+    }
+
+    public function respond(Request $request, callable $func) {
+        $params_unordered = $request->request->all();
+
+        $instance = $func[0];
+        $class = get_class($instance);
+        $method = $func[1];
+        $method_reflection = new \ReflectionMethod($class, $method);
+        $params = $method_reflection->getParameters();
+
+        $missing_parameters = [];
+        $params_ordered = [];
+        foreach($params as $p) {
+            $name = $p->getName();
+            $value = null;
+
+            if (!isset($params_unordered[$name])) {
+                if ($p->isOptional()) {
+                    $value = $p->getDefaultValue();
+                } else {
+                    $missing_parameters[] = $name;
+                }
+            } else {
+                $value = $params_unordered[$name];
+            }
+            $params_ordered[$name] = $value;
+        }
+
+        if ($missing_parameters) {
+            throw new Exception('Missing request parameters: ' . implode(',', $missing_parameters));
+        }
+
+        $ret = call_user_func_array($func, $params_ordered);
+
+        if (is_object($ret)){
+            throw new Exception("Cannot return object");
+        }
+        elseif(!is_array($ret)){
+            $ret =  ['result' => $ret];
+        }
+        return json_encode($ret);
+    }
+
+    public function error_response(Request $request, Exception $e) {
+
+    }
+}
